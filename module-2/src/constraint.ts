5cec296ec6fdc603b613d3f4f4255ace7ad642e3
SohelShahid117 <sohelshahid09@gmail.com>
2025-10-23 07:58:49 +0600

    2-7 Constraint using key of done

--- module-2/src/constraint.ts
+++ module-2/src/constraint.ts
@@ -16,4 +16,32 @@
     age: 33,
   });
   console.log(std1);
+
+  //2-7 Constraint using key of
+
+  type Vehicle = {
+    bike: string;
+    car: string;
+    ship: string;
+  };
+  type Owner = "bike" | "car" | "ship";
+  type Owner2 = keyof Vehicle;
+  const getPropertyValue = <X, Y extends keyof X>(obj: X, key: Y) => {
+    return obj[key];
+  };
+
+  const user = {
+    name: "Sohel",
+    age: 35,
+    address: "ctg",
+  };
+  const result1 = getPropertyValue(user, "age");
+  console.log(result1);
+
+  const car = {
+    model: "X",
+    year: 2022,
+  };
+  const result2 = getPropertyValue(car, "model");
+  console.log(result2);
 }
